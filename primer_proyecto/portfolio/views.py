bf6983e19b88bdcba8a18564f22bff140e1f9244
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-08-14 02:12:35 -0300

    Agrego autenticacion usuarios por pagina correctamente

--- primer_proyecto/portfolio/views.py
+++ primer_proyecto/portfolio/views.py
@@ -1,13 +1,27 @@
+import imp
+import os
+
 from django.shortcuts import render
 from .models import Proyectos
-import os
+#importamos el decorador para autenticacion de usuarios
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 #Recuperamos los registros de la tabla Proyectos que maneja nuestro modelo ORM a través de una lista de objetos interna y un método .all() que hace referencia a todos sus objetos.
 #Debemos inyectar estos proyectos en el template, para hacerlo simplemente enviamos a la función render un tercer parámetro con un diccionario y los valores que queremos inyectar.
 
+#usamos el decorador
+@login_required
 def portfolio(request):
+    """ Prueba autenticacion usuarios
+    from django.shortcuts import HttpResponse
+    html_response = "<h1>Proyectos/h1>"
+    return HttpResponse(html_response)
+    """
+
     projects = Proyectos.objects.all() 
     return render(request, os.path.join("portfolio","portfolio.html"), {'projects': projects})
 
+#La forma más fácil para restringir el acceso a tus funciones es aplicar login_required a tu función de vista. Si el usuario ha iniciado sesión entonces tu código de vista se ejecutará como normalmente lo hace. Si el usuario no ha iniciado sesión, se redirigirá a la URL de inicio de sesión definida en tu configuración de proyecto (settings.LOGIN_URL), pasando el directorio absoluto actual como el parámetro URL next.
